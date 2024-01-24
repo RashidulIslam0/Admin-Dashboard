42c1f8789e4f16d4e0544f7f79daaa768671b127
Rashidul <aboutrashidul@gmail.com>
2024-01-24 22:27:10 +0600

    navbar ui

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./home.scss";
 import Sidebar from "../../Components/sidebar/Sidebar";
+import Navbar from "../../Components/navbar/Navbar";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">containner</div>
+      <div className="homeContainer">
+        <Navbar />
+      </div>
     </div>
   );
 };
