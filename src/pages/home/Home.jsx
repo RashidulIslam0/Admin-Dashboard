5c27bba2c399a0e0c0430a89975f775a20c33ae8
Rashidul <aboutrashidul@gmail.com>
2024-01-24 23:02:01 +0600

    Reusable Widget Components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./home.scss";
 import Sidebar from "../../Components/sidebar/Sidebar";
 import Navbar from "../../Components/navbar/Navbar";
+import Widget from "../../Components/widget/Widget";
+
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
